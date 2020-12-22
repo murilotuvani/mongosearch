85cd77bddc2cbfcfb7d39e3176d455c7bdaf7353
Murilo Tuvani <murilo.tuvani@gmail.com>
2020-12-21 22:21:32 -0300

    https://start.spring.io/ create project

---
+++ src/main/java/com/example/mongosearch/MongosearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongosearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongosearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongosearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mongosearch/MongosearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongosearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongosearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
