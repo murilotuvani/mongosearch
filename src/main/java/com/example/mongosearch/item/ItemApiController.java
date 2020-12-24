4a643d40d53eb61bc022541e49fe1f6751513c80
Murilo Tuvani <murilo@autogeral.com.br>
2020-12-23 21:53:00 -0300

    Item API Controller to interact with the system without using the web interface

---
+++ src/main/java/com/example/mongosearch/item/ItemApiController.java
@@ -0,0 +1,89 @@
+package com.example.mongosearch.item;
+
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/item")
+@CrossOrigin(origins = "*")
+public class ItemApiController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(ItemApiController.class);
+
+	@Autowired
+	private ItemService itemService;
+	
+	@GetMapping("/id/{id}")
+	public ResponseEntity<Item> getById(@PathVariable String id) {
+		LOG.debug("getById{}", id);
+		ResponseEntity<Item> response = itemService.findById(id).map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+		return response;
+	}
+
+	/**
+	 * curl --header "Content-Type: application/json" http://localhost:8080/api/item
+	 * 
+	 * @return
+	 */
+	@GetMapping
+	public ResponseEntity<List<Item>> list() {
+		LOG.debug("list()");
+		List<Item> list = itemService.list();
+		return ResponseEntity.ok(list);
+	}
+
+	/**
+	 * 
+	 * 	String title;
+	String description;
+	BigDecimal price;
+	Long ean13;
+	
+	
+	 curl --header "Content-Type: application/json" \
+	      --request POST \
+	      --data '{"title": "Soda Diet Coke Cube Pack","description": "Soda Diet Coke Cube Pack.","price": 0.95, "ean13": 49000010633}' \
+	      http://localhost:8080/api/item
+	 * 
+	 * @param item
+	 * @return
+	 */
+	@PostMapping
+	public ResponseEntity<Item> save(@RequestBody Item item) {
+		LOG.debug("save({})", item);
+		Item opItem = itemService.save(item);
+		// Ok?
+		return ResponseEntity.ok(opItem);
+
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<Item> delete(@RequestBody Item item) {
+		LOG.debug("delete({})", item);
+		itemService.delete(item);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping
+	public ResponseEntity<Item> update(@RequestBody Item item) {
+		LOG.debug("update({})", item);
+		Item nItem = itemService.save(item);
+		return ResponseEntity.ok(nItem);
+	}	
+
+}
--- src/main/java/com/example/mongosearch/item/ItemService.java
+++ src/main/java/com/example/mongosearch/item/ItemService.java
@@ -1,6 +1,7 @@
 package com.example.mongosearch.item;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,21 @@ public class ItemService {
 	@Autowired
 	ItemMongoRepository itemMongoRepository;
 
-	public void save(Item item) {
-		itemMongoRepository.save(item);
+	
+	public Optional<Item> findById(String id) {
+		return itemMongoRepository.findById(id);
+	}
+	
+	public Item save(Item item) {
+		return itemMongoRepository.save(item);
 	}
 	
 	public List<Item> list() {
 		return itemMongoRepository.findAll();
+	}
+
+	public void delete(Item item) {
+		itemMongoRepository.delete(item);
 	}
 
 }
